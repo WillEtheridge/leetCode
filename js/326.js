0fb3d7d03978f0fa43b11271073d30917a2de998
WillEtheridge <will@originthread.com>
2024-08-14 10:09:53 +0100

    326

---
+++ js/326.js
@@ -0,0 +1,66 @@
+/*
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+Example 1:
+
+Input: n = 27
+Output: true
+Explanation: 27 = 33
+
+Example 2:
+
+Input: n = 0
+Output: false
+Explanation: There is no x where 3x = 0.
+
+Example 3:
+
+Input: n = -1
+Output: false
+Explanation: There is no x where 3x = (-1).
+
+*/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+
+// Attempt 1 - Slow!
+var isPowerOfThreeSlow = function(n) {
+    if (n === 1) {
+        return true;
+    }
+    if (n < 3) {
+        return false;
+    } 
+    while (n > 0) {
+        if ( n === 3) {
+            return true;
+        }
+        n = n/3
+        console.log(n)
+        if (!Number.isInteger(n)) {
+            return false;
+        }
+    }
+};
+
+// Attempt 2 - Faster
+var isPowerOfThree = function(n) {
+    if (n === 1 ) {
+        return true;
+    }
+
+    if (n % 3 !== 0 || n === 0) {
+        return false;
+    }
+
+    return isPowerOfThree(n/3)
+}
+
+
+
+console.log(isPowerOfThree(27))
